3f474a69297b2e26f99f9f58b9e2a2b3cbcadd92
Jesse McKay and Levi Bibo <jesse@>
2016-10-27 15:22:31 -0700

    remove spaces from original sentence, turn it into an array, then using the calculated grid dimensions loop through and push each element into a new array as it would appear in the grid.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 function encoder(inputString) {
-  var rootLength = Math.sqrt(inputString.length);
+  var inputStringNoSpaces = inputString.split(" ").join("");
+  var inputArrayNoSpaces = inputStringNoSpaces.split("");
+  var rootLength = Math.sqrt(inputStringNoSpaces.length);
   var rootLengthDec = rootLength % 1;
   rootLength -= rootLengthDec;
   var gridDimensions = getDimensions(rootLengthDec, rootLength);
+  var filledGrid = fillGrid(gridDimensions, inputArrayNoSpaces);
+  return filledGrid;
+}
+
+function fillGrid(gridDimensions, inputArrayNoSpaces) {
+  var filledArray = [];
+  for (y = 0; y < gridDimensions[1]; y++) {
+    filledArray.push([]);
+    for (x = 0; x < gridDimensions[0]; x++) {
+      filledArray[y].push(inputArrayNoSpaces[0]);
+      inputArrayNoSpaces.shift();
+    }
+  }
+  return filledArray;
 }
 
 function getDimensions(rootLengthDec, rootLength) {
@@ -28,6 +44,6 @@ function getDimensions(rootLengthDec, rootLength) {
 $(document).ready(function() {
   $("form").submit(function(event) {
      event.preventDefault();
-     $("output").text(encoder($("#input").val()));
+     $("#output").text(encoder($("#input").val()));
   });
 });
